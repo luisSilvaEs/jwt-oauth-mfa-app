ed078515d5bad5725e3a2bd846cf1eefdfbe617a
Luis Silva <siel_alb@hotmail.com>
2026-05-07 15:50:07 -0600

    chore: created backend start structure through VCode

---
+++ backend/jwt-oauth-mfa-app/src/main/java/com/luissilvacoding/jwt_oauth_mfa_app/JwtOauthMfaAppApplication.java
@@ -0,0 +1,13 @@
+package com.luissilvacoding.jwt_oauth_mfa_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtOauthMfaAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtOauthMfaAppApplication.class, args);
+	}
+
+}
---
+++ backend/jwt-oauth-mfa-app/src/test/java/com/luissilvacoding/jwt_oauth_mfa_app/JwtOauthMfaAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luissilvacoding.jwt_oauth_mfa_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtOauthMfaAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
