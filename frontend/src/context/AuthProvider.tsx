0f6f2b4c89eb84056df11a1ba676827c05c58df6
Luis Silva <siel_alb@hotmail.com>
2026-05-14 13:33:28 -0600

    feat: create auth context

---
+++ frontend/src/context/AuthProvider.tsx
@@ -0,0 +1,38 @@
+import { useContext, useEffect, useState } from "react";
+import type { ReactNode } from "react";
+import type { User } from "../types/auth";
+import AuthContext from "./AuthContext";
+
+export function AuthProvider({ children }: { children: ReactNode }) {
+  const [token, setToken] = useState<string | null>(
+    localStorage.getItem("token"),
+  );
+
+  const [user, setUser] = useState<User | null>(null);
+
+  function login(token: string, user: User) {
+    localStorage.setItem("token", token);
+    setToken(token);
+    setUser(user);
+  }
+
+  function logout() {
+    localStorage.removeItem("token");
+    setToken(null);
+    setUser(null);
+  }
+
+  return (
+    <AuthContext.Provider
+      value={{
+        token,
+        user,
+        login,
+        logout,
+        isAuthenticated: !!token,
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+}
